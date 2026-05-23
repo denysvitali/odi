5e8d97ce5aeef6f347b57dafcc7b6e91fa35887e
Denys Vitali <denys@denv.it>
2026-05-23 15:32:08 +0200

    Set GH Pages base path for /odi

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.UI_BASE_PATH || '/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
